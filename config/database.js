0558a0ae13d77fc6da74b6f982e8849904522cae
vaggy <sarvagya6@gmail.com>
2021-11-11 05:46:41 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,6 +2,26 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
+      connector: 'mongoose',
+      settings: {
+        uri: env('DATABASE_URI'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+})
+
+/*
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
@@ -13,7 +33,7 @@ module.exports = ({ env }) => ({
     },
   },
 });
-/*
+
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
